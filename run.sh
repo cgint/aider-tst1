e4428186b347373136ad3dc54ac09d6ef1182de0
Christian Gintenreiter <christian.gintenreiter@gmail.com>
2023-12-01 22:34:51 +0100

    aider: Update base directory in Dockerfile to '/app2'
    # Aider chat conversation:
    
    USER: Adapt the files so that base directory of '/usr/app' is changed to '/app2'
    
    ASSISTANT: No changes are needed.

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 docker build -t solution-image .
 
 # Run the Docker container with volume mounts
-docker run --rm --name solution-container -v "$(pwd)/input:/usr/app/input" -v "$(pwd)/output:/usr/app/output" solution-image
+docker run --rm --name solution-container -v "$(pwd)/input:/app2/input" -v "$(pwd)/output:/app2/output" solution-image
